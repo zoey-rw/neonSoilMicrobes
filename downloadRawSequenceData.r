e350463754e17444a7f1b2d1411cbb75e4cf9a72
Zoey Werbin <zoeywerbin@gmail.com>
2020-01-28 12:25:16 -0500

    updating function for downloading raw sequence data files - now checks for previously downloaded TAR and FASTQ files, and uses a better approach for renaming files

--- downloadRawSequenceData.r
+++ downloadRawSequenceData.r
@@ -1,121 +1,138 @@
-# Function to download 16S raw sequencing data from NEON soil samples. 
+# Function to download 16S and ITS raw sequencing data from NEON soil samples. 
 #
 # Initially written by Lee Stanish, NEON; further adapted by Zoey Werbin
 # Will not work for legacy data.
 #
-# for testing:
-startdate="2016-01"
-enddate="2019-07"
-check.size=F
-site="CPER"
-outdir="/projectnb/talbot-lab-data/zrwerbin/NEON_16S_data_construction/raw_sequences_16S/recent/"
-amplicon="16S"
+#downloadRawSequenceData(out.dir = "/projectnb/talbot-lab-data/zrwerbin/NEON_16S_ITS_data_construction/testing/raw_seqs/ITS", startdate="2016-01", enddate="2016-04")
 
-downloadRawSequenceData(site="CPER", startdate="2013-01", enddate="2019-01", check.size=F, outdir="/projectnb/talbot-lab-data/zrwerbin/NEON_16S_data_construction/raw_sequences_16S/recent/", amplicon = "16S")
-downloadRawSequenceData(site="HARV", startdate="2013-01", enddate="2019-01", check.size=F, outdir="/projectnb/talbot-lab-data/zrwerbin/NEON_16S_data_construction/raw_sequences_16S/recent/", amplicon = "16S")
-downloadRawSequenceData(site="BART", startdate="2013-01", enddate="2019-01", check.size=F, outdir="/projectnb/talbot-lab-data/zrwerbin/NEON_16S_data_construction/raw_sequences_16S/recent/", amplicon = "16S")
+site = c("CPER","DSNY","HARV","OSBS","STER"); startdate="2015-06"; enddate="2019-06"; out.dir = "/projectnb/talbot-lab-data/zrwerbin/NEON_16S_ITS_data_construction/testing/raw_seqs/ITS"; check.size=F; tar_dir = "/projectnb/talbot-lab-data/zrwerbin/NEON_16S_ITS_data_construction/data/ITS/rawSeqs/recent/tar/";  amplicon="ITS"; fastq_dir = NULL; tar_dir = NULL
 
-downloadRawSequenceData(site="all", startdate="2013-01", enddate="2019-01", check.size=F, outdir="/projectnb/talbot-lab-data/zrwerbin/NEON_16S_data_construction/raw_sequences_16S/recent/", amplicon = "16S")
+downloadRawSequenceData <- function(site = "HARV", startdate="YYYY-MM", enddate="YYYY-MM", check.size=F, amplicon="ITS", out.dir= NULL, fastq_dir = NULL, tar_dir = NULL) {
 
-downloadRawSequenceData <- function(site="all", startdate="YYYY-MM", enddate="YYYY-MM", outdir="", check.size=TRUE, 
-                                    amplicon = "16S", ...){
   
   if (!require(neonUtilities)) install.packages('neonUtilities')
   if(amplicon != "16S" & amplicon != "ITS"){
-    stop("Value of 'amplicon' must be '16S' or 'ITS'. 16S sequencing targets bacterial and archaeal sequences, while ITS sequencing targets fungal sequences. Not currently tested for ITS data.")
+    stop("Value of 'amplicon' must be '16S' or 'ITS'. 16S sequencing targets bacterial and archaeal sequences, while ITS sequencing targets fungal sequences.")
   }
   
-  #### CREATE OUTPUT DESTINATIONS ####
-  fastq_dir <- file.path(outdir,"fastq/")
-  tar_dir <- file.path(outdir,"tar/")
-  if(!dir.exists(fastq_dir))  dir.create(fastq_dir)
-  if(!dir.exists(tar_dir))    dir.create(tar_dir)
+  
+#### CREATE OUTPUT DESTINATIONS ####
+if (is.null(out.dir)) out.dir <- getwd()
+if (!dir.exists(out.dir)) dir.create(out.dir, showWarnings = FALSE)
+if (is.null(fastq_dir)) fastq_dir <- file.path(out.dir, "fastq/")
+if (is.null(tar_dir)) tar_dir <- file.path(out.dir, "tar/")
+if (!dir.exists(fastq_dir))  dir.create(fastq_dir, showWarnings = FALSE)
+if (!dir.exists(tar_dir)) dir.create(tar_dir, showWarnings = FALSE)
 
-  #### RETRIEVE DOWNLOAD URLS AND METADATA FROM NEON ####
-  
-  # load amplicon sequencing metadata into workspace
-  dat <- neonUtilities::loadByProduct(dpID="DP1.10108.001", site=site, startdate=startdate, enddate=enddate, package="expanded", check.size=check.size)
-  
-  # extract 16S or ITS marker gene sequencing metadata.
-  sequencing.dat <- dat[[paste0("mmg_soilMarkerGeneSequencing_", amplicon)]]
+#### RETRIEVE DOWNLOAD URLS AND METADATA FROM NEON ####
 
-  # clean up some FASTQ filenames
-  sequencing.dat$files_we_want <- gsub(".gz","", sequencing.dat$processedSeqFileName)
-  sequencing.dat$files_we_want <- gsub("\\_fastq", "\\.fastq", sequencing.dat$files_we_want)
-  
-  # get unique tar download URLs, and subset by amplicon type.
-  u.urls <- unique(dat$mmg_soilRawDataFiles$rawDataFilePath)
-  u.urls <- u.urls[grepl(amplicon, u.urls, fixed = TRUE)]
-  
-  # get tar file names
-  fileNms <- gsub('^.*\\/', "", u.urls)
-  cat(paste0("There are ", length(u.urls), " unique files to download.\n")) 
-  
-  for(i in 1:length(u.urls)) { # loop through each URL
-    #for(i in 2:3) { # for testing
+# Load amplicon sequencing metadata into workspace
+dat <- neonUtilities::loadByProduct(dpID = "DP1.10108.001", site = site, startdate = startdate,
+    enddate = enddate, package = "expanded", check.size = check.size)
+
+# Extract soil raw file metadata.
+sequencing.dat <- dat[["mmg_soilRawDataFiles"]]
+# Subset by amplicon type.
+sequencing.dat <- sequencing.dat[grep(amplicon, sequencing.dat$rawDataFileName), ]
+# Add useful columns
+sequencing.dat$which.reads <- substr(sequencing.dat$rawDataFileDescription, 1, 2) # get "R1" or "R2" from column description
+sequencing.dat$new_names <-  paste0(sequencing.dat$dnaSampleID,  "_", amplicon, "_", sequencing.dat$which.reads, ".fastq")
+
+# Preferentially remove duplicates that don't have an internalLabID
+sequencing.dat <- sequencing.dat[order(sequencing.dat$internalLabID),]
+sequencing.dat <- sequencing.dat[!duplicated(sequencing.dat$new_names),]
+
+# Grab internalLabID from other dataframe if needed
+other.dat <- dat[[paste0("mmg_soilMarkerGeneSequencing_", amplicon)]]
+missing.internalID <- sequencing.dat[is.na(sequencing.dat$internalLabID),]$dnaSampleID
+fix.internalID <- other.dat[other.dat$dnaSampleID %in% missing.internalID, c("dnaSampleID", "internalLabID")]
+colnames(fix.internalID)[2] <- "newinternalID"
+sequencing.dat <- merge(sequencing.dat, fix.internalID, by = "dnaSampleID", all.x=T)
+sequencing.dat$internalLabID <- ifelse(is.na(sequencing.dat$internalLabID), sequencing.dat$newinternalID, sequencing.dat$internalLabID)
+sequencing.dat <- sequencing.dat[!is.na(sequencing.dat$internalLabID ),]
+
+# create file names to search against our already-downloaded files
+sequencing.dat$fastqFileNames <- paste0(sequencing.dat$internalLabID, "_", amplicon, "_", sequencing.dat$which.reads, ".fastq")
+
+
+# get unique tar download URLs
+u.urls <- sort(unique(sequencing.dat$rawDataFilePath))
+n.urls <- length(u.urls)
+# get tar file names
+fileNms <- gsub('^.*\\/', "", u.urls)
+seqRun <- sapply(strsplit(fileNms, split = "_"), '[', 2)
+cat(paste0("\nDesired fastQ files are split into ", n.urls, " unique sequencing runs (tar.gz files) to download.\nIf these have previously been downloaded, set the `tar_dir` argument to the previous download location.\n"))
+
+
+#### LOOP THROUGH SEQUENCING RUNS AND DOWNLOAD DESIRED FILES ####
+
+for (i in 1:length(u.urls)) {
+#  for (i in 10:10) {
     
-    # search for the tar file name in the provided out-directory
-    file_exists <- grepl(fileNms[i], list.files(tar_dir), fixed = TRUE)
-    if(any(file_exists)) { cat(paste0("File: ", fileNms[i], " already exists. Skipping download.\n"))
-
-    } else {
-      
-      #### DOWNLOAD TAR FILES AND EXTRACT THE SAMPLES WE WANT ####
-      utils::download.file(url=as.character(u.urls[i]), destfile = paste0(tar_dir, fileNms[i]))
-      cat(paste0("File: ", fileNms[i], " has been downloaded.\n"))
-      
-    }
-        # look into compressed tar files and get the list of FASTQ file names
-        files_in_tar <- utils::untar(tarfile = paste0(tar_dir, fileNms[i]), list = T)
-        files_we_have <- gsub("\\_R2|\\_R1", "", files_in_tar)
-        
-        # now subset the FASTQ files to only those which are in our metadata
-        files_to_extract <- files_in_tar[which(basename(files_we_have) %in% sequencing.dat$files_we_want)]
-        # extract and decompress those FASTQ files into a new "decompressed" subdirectory. 
-        # this can take a minute.
-        if (length(files_to_extract) > 0) {
-          cat("Extracting files...")
-          utils::untar(tarfile = paste0(tar_dir, fileNms[i]), 
-                files = files_to_extract,
-                exdir = paste0(fastq_dir))
-          cat(paste0("File: ", fileNms[i], " has been extracted.\n"))
-          
-          
-          #### FIX FILENAMES (i.e. to SITE_PLOT_HORIZON_CORE_DATE_DNA1_R1.fastq) ####
-          
-          # create sample names with R1 or R2 prefix 
-          R1 <- grep("\\_R1", files_to_extract)
-          R2 <- grep("\\_R2", files_to_extract)
-          sequencing.dat$new_names <- gsub("\\-GEN", "", sequencing.dat$dnaSampleID) # don't need the "GEN" suffix 
-          if (length(R1) > 0) {
-            sequencing.dat$new_names <- paste0(sequencing.dat$new_names,"_R1.fastq")
-          } else if(length(R2) > 0) {
-            sequencing.dat$new_names <- paste0(sequencing.dat$new_names,"_R2.fastq")} # this is not a solid way of appending R1/R2
-          
-          # find the metadata row that matches our downloaded sample files
-          new_names <- sequencing.dat[charmatch(substr(basename(files_to_extract), 1, 19), sequencing.dat$processedSeqFileName),]$new_names
-          
-          # create old/new file names and rename
-          old.names <- paste0(outdir, "fastq/", files_to_extract)
-          new.names <- paste0(outdir, "fastq/", dirname(files_to_extract), "/", new_names)
-          file.rename(from = old.names, to = new.names)
-          
-          
-          #### CLEAN UP TIME! ####  
-          
-          # moves nested files out of their folders, and into a new folder called "to_keep"
-          cmd <- paste0("find ", fastq_dir, " -mindepth 2 -type f -exec mv -i -f '{}' ", fastq_dir, " ';'")
-          system(cmd)
-          # now let's remove only the empty directories
-          cmd <- paste0("rmdir -p ", fastq_dir, unique(dirname(files_to_extract)))
-          system(cmd) # ignore the error here! (I don't know how to avoid it)
-          
-          cat("The following samples have been renamed and are in the directory 'fastq/':")
-          print(basename(new.names))
-          
-        } else {
-          cat("No fastq files to extract.")
-        } # close conditional extraction of new/wanted fastq files
-    } # close loop through URLs
-  } ## close function ##
+    # check if file needs to be downloaded
+  file_exists <- grepl(fileNms[i], list.files(tar_dir), fixed = TRUE)
+  if (any(file_exists)) {
+    cat(paste0("\nRun: ", i, "/", n.urls, ": ", fileNms[i], " already exists. Skipping download.\n"))
+  } else {
+    
+    #### DOWNLOAD tar files ####
+    utils::download.file(url = as.character(u.urls[i]),
+                         destfile = paste0(tar_dir, fileNms[i]))
+    cat(paste0("\nRun: ", i, "/", n.urls, ": ", fileNms[i], " has been downloaded to:\n", paste0(tar_dir, fileNms[i])))
+  }
   
+  # Create output directory for fastQ files from this run
+  fastq.out.dir <- paste0(fastq_dir, seqRun[i])
+  dir.create(fastq.out.dir, showWarnings = FALSE)
+  
+  # Get overlap between our tar files and our metadata files
+  files.in.tar <-  utils::untar(tarfile = paste0(tar_dir, fileNms[i]), list = T)
+  search.list  <-  paste0(sequencing.dat$fastqFileNames, collapse = "|")
+  files.to.extract <- files.in.tar[grep(search.list, files.in.tar)]
+  
+  # Get names of files we've already downloaded; remove from search list
+  already_have <- list.files(fastq.out.dir, recursive = T)
+  already_have_processed <- already_have[grepl("-GEN-DNA", already_have)] # subset to the *new* files from this run.
+  already_have_skip <- sequencing.dat[sequencing.dat$new_names %in% already_have_processed,]$fastqFileNames
+  files.to.extract <- files.to.extract[!basename(files.to.extract) %in% already_have_skip]
+  if(length(files.to.extract) == 0) next()
+  
+  # Extract select files from tar
+  utils::untar(
+    tarfile = paste0(tar_dir, fileNms[i]),
+    files = files.to.extract,
+    exdir = fastq.out.dir
+  )
+  
+  cat(paste0("\nFiles from run: ", i, "/", n.urls, ": ", fileNms[i], " have been extracted.\n"))
+  
+  # Move nested files into a new folder
+  cmd <-  paste0("find ", fastq.out.dir, " -mindepth 2 -type f -exec mv -i -f '{}' ", fastq.out.dir, " ';'");  system(cmd)
+  # Now remove only the empty directories
+  cmd <- paste0("cd ", fastq.out.dir, "; find . -type d -empty -delete"); system(cmd) 
+  
+  #### FIX FILENAMES (i.e. to SITE_PLOT_HORIZON_CORE_DATE_DNA1_R1.fastq) ####
+  
+  files_extracted <- basename(files.to.extract)
+  samples_extracted <- gsub("_ITS_R[12].fastq|_16S_R[12].fastq", "", files_extracted)
+  
+  # Subset file metadata to samples we have, and reorder
+  file.data <-  sequencing.dat[which(sequencing.dat$fastqFileNames %in% files_extracted), ] # subset file metadata to our DNA samples
+  file.data <- file.data[order(match(file.data$fastqFileNames, files_extracted)), ] # match order
+  
+  # Create old/new file names and rename
+  new.names <- file.path(fastq.out.dir, file.data$new_names)  
+  old.names <- file.path(fastq.out.dir, files_extracted)
+  if (nrow(file.data) > 0 & 
+      (length(old.names) == length(new.names)) &
+      all(file.data$fastqFileNames==files_extracted) &
+      all(unlist(lapply(old.names, file.exists)))) { 
+          file.rename(from = old.names, to = new.names)
+          cat(paste0("\nDownloaded the following files:\n", paste0(new_names, collapse="\n")))
+  } else cat(paste0("\nFile renaming not successful for run: ", i, "/", n.urls, ": ", fileNms[i]))
+  
+  # remove any NA files - shouldn't need this anymore
+  # system(paste0("rm ", fastq.out.dir, "/NA"))
+}
+cat("\n\nFile download complete.")
+}
