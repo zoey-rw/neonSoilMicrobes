67e8137d6f2ee687f0216fd32c81ce7471652e69
Zoey Werbin <zoeywerbin@gmail.com>
2020-04-02 21:14:14 -0400

    changed some commentary

--- qualityFilter.r
+++ qualityFilter.r
@@ -1,5 +1,4 @@
-# not yet tested for ITS
-# only uses forward reads for ITS
+# this could be streamlined/generalized more 
 
 qualityFilter <- function(trimmed.seq.dir, out.dir, amplicon = "16S", fwdEE=5, revEE=5){
   fnFs <- sort(list.files(trimmed.seq.dir, pattern = "_R1.fastq.gz|_R1.fastq", full.names = TRUE)) # primer-trimmed FWD files
@@ -25,7 +24,7 @@ qualityFilter <- function(trimmed.seq.dir, out.dir, amplicon = "16S", fwdEE=5, r
     else next
   }
   
-  # remove any samples that have low-quality reads early on, to avoid spoiling the whole run
+  # remove any samples that have low-quality reads early on, to avoid unnecessarily tossing the whole run
   # idk how to loop this command
   to_remove <- which(unlist(fwd.trunc.lengths) < 100 | unlist(rev.trunc.lengths) < 100)
   if (length(to_remove)>0){
